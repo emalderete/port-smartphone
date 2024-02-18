8f20bfd655d85d3c7d18ef8d44b2ebe1e69500a9
emalderete <nicolas.alderete95@live.com>
2024-02-18 02:14:12 -0300

    Desarrollado componente Reloj
    • Desarrollado el componente reloj y agregados algunos estilos para mostrar en el sitio.

--- app/components/ui/Clock.jsx
+++ app/components/ui/Clock.jsx
@@ -1,7 +1,42 @@
+"use client";
+import { useState, useEffect } from 'react';
+import styles from './clock.module.css';
+
 function Clock() {
+    const [time, setTime] = useState([]);
+
+    useEffect(()=>{
+        // Lógica del reloj
+        function currentTime() {
+            const currentDate = new Date();
+            var hours = currentDate.getHours();
+            var minutes = currentDate.getMinutes();
+
+            if(hours < 10) {
+                var hours = '0' + hours;
+            }
+            if(minutes < 10) {
+                var minutes = '0' + minutes;
+            }
+            var timeArray = [hours, minutes];
+
+            if(JSON.stringify(time) !== JSON.stringify(timeArray)){
+                setTime(timeArray);
+            }
+        }
+
+        currentTime();
+
+        const updateInterval = setInterval((currentTime), 1000);
+
+        return () => {clearInterval(updateInterval)}
+    }, [time]);
+
     return (
-        <div>
-            
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.time}>{time.join(':')}</h1>
+            </div>
         </div>
     );
 }
