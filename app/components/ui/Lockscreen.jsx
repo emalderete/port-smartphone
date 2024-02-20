3d0bb2f8f52385e95fba13ba28c5bacebdee8c3c
emalderete <nicolas.alderete95@live.com>
2024-02-20 01:44:16 -0300

    Desarrollado componente Reminders
    • Desarrollada la lógica del componente reminders. Aunque no está completa ya que debería guardarse en algún lado lo que se tilde o destilde pero al menos visualmente funciona.
    
    • Desarrollada la estética del mismo componente.
    
    • Agregado nuevo contenedor lateral para mostrar los widgets.
    
    • El contenedor principal se divide en 2 para tener un área dedicada a la hora específicamente y otra para las notificaciones.

--- app/components/ui/Lockscreen.jsx
+++ app/components/ui/Lockscreen.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import TwilightBG from '../../../public/ui/twilight-bg.jpg';
 import Clock from "./Clock";
+import Reminders from "./Reminders";
 import styles from './lockscreen.module.css';
 
 function Lockscreen() {
@@ -8,7 +9,17 @@ function Lockscreen() {
         <div className={styles.lockscreen}>
             <Image className={styles.lockscreen_bg} src={TwilightBG} alt="alt" width={3000} height={2000} />
             <div className={styles.overlay}>
-                <Clock></Clock>
+                <aside className={styles.aside}>
+                    <Reminders></Reminders>
+                </aside>
+                <div className={styles.content}>
+                    <div className={styles.clockSpace}>
+                        <Clock></Clock>
+                    </div>
+                    <div className={styles.notificationArea}>
+
+                    </div>
+                </div>
             </div>
         </div>
     );
--- app/components/ui/Reminders.jsx
+++ app/components/ui/Reminders.jsx
@@ -1,7 +1,38 @@
+'use client';
+import { useRef, useEffect } from 'react';
+import styles from './reminders.module.css';
+
 function Reminders() {
+    const reminder_task_ref_1 = useRef(null);
+    const reminder_task_ref_2 = useRef(null);
+    const reminder_task_ref_3 = useRef(null);
+    const reminder_task_ref_4 = useRef(null);
+
+    useEffect( () => {
+        var tasks = [reminder_task_ref_1.current, reminder_task_ref_2.current, reminder_task_ref_3.current, reminder_task_ref_4.current];
+
+        tasks.forEach( element => {
+            element.addEventListener('click', () => {
+                if(element.checked){
+                    console.log('El elemento: ' + element.value + ' fue chequeado');
+                } else {
+                    console.log('El elemento: ' + element.value + ' no está chequeado');
+                }
+            })
+        });
+    }, [])
+
     return (
-        <div>
-            
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <h4 className={styles.title}>Recordatorios</h4>
+                <ul className={styles.list}>
+                    <li className={styles.item}><input ref={reminder_task_ref_1} id='reminder-task_1' className={styles.check} type='checkbox' value='limpieza' /> <label htmlFor='reminder-task_1'>Limpieza diaria de la habitación</label></li>
+                    <li className={styles.item}><input ref={reminder_task_ref_2} id='reminder-task_2' className={styles.check} type='checkbox' value='lavar_ropa' /> <label htmlFor='reminder-task_2'>Lavar la ropa</label></li>
+                    <li className={styles.item}><input ref={reminder_task_ref_3} id='reminder-task_3' className={styles.check} type='checkbox' value='programar' /> <label htmlFor='reminder-task_3'>Programar</label></li>
+                    <li className={styles.item}><input ref={reminder_task_ref_4} id='reminder-task_4' className={styles.check} type='checkbox' value='alimentar_gato' /> <label htmlFor='reminder-task_4'>Alimentar al gato</label></li>
+                </ul>
+            </div>
         </div>
     );
 }
