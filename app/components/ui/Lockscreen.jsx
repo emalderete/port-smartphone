c954079490a176bc133a7fc3c8f5214a9c67822d
emalderete <nicolas.alderete95@live.com>
2024-02-27 23:27:18 -0300

    Modificado componente del reloj
    Se le agrega un círculo levemente iluminado con blur para realzar el efecto.

--- app/components/ui/Lockscreen.jsx
+++ app/components/ui/Lockscreen.jsx
@@ -1,19 +1,21 @@
 import Image from "next/image";
-import TwilightBG from '../../../public/ui/twilight-bg.jpg';
+import twilightBG from "@/public/ui/twilight-bg.jpg";
 import Clock from "./Clock";
 import Reminders from "./Reminders";
 import Notifications from "./Notifications";
 import CurrentDate from "./CurrentDate";
+import Weather from "./Weather";
 import styles from './lockscreen.module.css';
 
 function Lockscreen() {
     return (
         <div className={styles.lockscreen}>
-            <Image className={styles.lockscreen_bg} src={TwilightBG} alt="alt" width={3000} height={2000} />
+            <Image className={styles.lockscreen_bg} src={twilightBG} alt="alt" width={3000} height={2000} />
             <div className={styles.overlay}>
                 <aside className={styles.aside}>
                     <Reminders></Reminders>
                     <CurrentDate></CurrentDate>
+                    <Weather></Weather>
                 </aside>
                 <div className={styles.content}>
                     <div className={styles.clockSpace}>
