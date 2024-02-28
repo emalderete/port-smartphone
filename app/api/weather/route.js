bc089cd541ee6bc7db937219c0dd973c715cef70
emalderete <nicolas.alderete95@live.com>
2024-02-27 23:22:25 -0300

    Desarrollada api para consultar el clima
    El desarrollo se realizó del lado del servidor para ocultar la apikey

--- app/api/weather/route.js
+++ app/api/weather/route.js
@@ -1,7 +1,18 @@
-import { NextResponse } from 'next/server';
+export async function POST(request){
+    const res = await request.json();
+    let lat = res.latitude;
+    let lon = res.longitude;
 
-export async function GET(request){
-    return NextResponse.json({ msj: 'Api consumida correctamente' }, { status: 200});
+    const openWeatherApiKey = '1dedb490825caf835053c9f0e16a2116';
+    const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=${openWeatherApiKey}`;
+
+    const apiRes = await fetch(api, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    const jsonData = await apiRes.json();
+
+    return Response.json({status: 200, data: jsonData, message: 'Solicitud aprobada'});
 }
-
-// Recuerda definir la api de clima, consumirla y entregar los datos con esta api.
